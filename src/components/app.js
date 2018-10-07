361141035b49d754376430128efcaa0002f23516
Bob Polacheck <rpolacheck@gmail.com>
2018-10-07 21:32:12 +0000

    Added components

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Note To Self</h2>
+            </div>    
+        )
+    }
+}
+
+export default App;
