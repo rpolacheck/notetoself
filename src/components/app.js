7591dbb2ca5f4f7a51eb994c047f01f03ad05bce
Bob Polacheck <rpolacheck@gmail.com>
2018-10-07 22:25:53 +0000

    Added State to our form and cleaned our index page

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+
 
 class App extends Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            text: ''
+        };
+    }
+    
     render() {
         return (
             <div>
                 <h2>Note To Self</h2>
+                <Form inline>
+                    <FormControl onChange={event => this.setState({ text: event.target.value })} />
+                    {' '}
+                    <Button onClick={() => console.log(this.state)}>Submit</Button>
+                </Form>
             </div>    
-        )
+        );
     }
 }
 
